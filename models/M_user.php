4115b9c6e10477b9d7b261525fb107060b02ec50
mdnklbrg <mdinkelberg@outlook.com>
2015-10-01 13:21:22 +0200

    bla +  validation added

--- models/M_user.php
+++ models/M_user.php
@@ -10,10 +10,20 @@ class user
         $json = json_encode($users);
         return $json;
     }
+
 	public function insert($db, $post)
 	{
 		$sql = "INSERT INTO users(`email`, `first_name`, `last_name`, `password`) VALUES('".$post['email']."', '".$post['firstname'] ."', '".$post['lastname'] ."', '".$post['password'] ."')";
 		$db->insert_update($sql);
 		print_r($_POST);
 	}
+
+    public function getOne($db, $id)
+    {
+        $sql = "SELECT * FROM users WHERE id = $id";
+        $result = $db->fetch($sql);
+        $json = json_encode($result);
+
+        return $json;
+    }
 }
